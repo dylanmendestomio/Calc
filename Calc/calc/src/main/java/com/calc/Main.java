19fc8d69c8f0424c122427889e7dc4832a80bbc8
Dylan <dylanmendestomio23@gmail.com>
2025-08-05 19:46:59 -0300

    Sua mensagem de commit

--- Calc/calc/src/main/java/com/calc/Main.java
+++ Calc/calc/src/main/java/com/calc/Main.java
@@ -1,10 +1,41 @@
 package com.calc;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 5;
 
-        System.out.println(num1 + num2);
+        Scanner input = new Scanner(System.in);
+
+        double num1, num2, result;
+        char sign;
+
+        System.out.println("First number: ");
+        num1 = input.nextInt();
+
+        System.out.println("Number sign (+, -, * or /): ");
+        sign = input.next().charAt(0);
+
+        System.out.println("Second number: ");
+        num2 = input.nextInt();
+
+        if(sign == '+'){
+            result = num1 + num2;
+            System.out.println("Result: " + result);
+        }
+         if(sign == '-'){
+            result = num1 - num2;
+            System.out.println("Result: " + result);
+        }
+         if(sign == '*'){
+            result = num1 * num2;
+            System.out.println("Result: " + result);
+        }
+         if(sign == '/'){
+            result = num1 / num2;
+            System.out.println("Result: " + result);
+        }
+
+        input.close();
     }
 }
