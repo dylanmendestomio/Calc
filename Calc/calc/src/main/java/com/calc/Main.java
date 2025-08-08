90f5c8a012532ca388841e4ecd358116328599c6
Dylan <dylanmendestomio23@gmail.com>
2025-08-08 12:29:42 -0300

    Validation with while

--- Calc/calc/src/main/java/com/calc/Main.java
+++ Calc/calc/src/main/java/com/calc/Main.java
@@ -10,15 +10,26 @@ public class Main {
         double num1, num2, result;
         char sign;
 
-        System.out.println("First number:");
+        System.out.println("First number: ");
+        while (!input.hasNextDouble()) {
+            System.out.println("Invalid number. Try again: ");
+            input.next();
+        }
         num1 = input.nextInt();
 
-        System.out.println("Number sign (+, -, * or /):");
+        System.out.println("Number sign (+, -, * or /): ");
         sign = input.next().charAt(0);
+         while (sign != '+' && sign != '-' && sign != '*' && sign != '/') {
+            System.out.println("Invalid sign. Only (+, -, * or /): ");
+            sign = input.next().charAt(0);
+        }
 
-        System.out.println("Second number:");
+        System.out.println("Second number: ");
         num2 = input.nextInt();
 
+        if(sign == '^'){
+
+        }else{
         switch (sign) {
             case '+':
                 result = num1 + num2;
@@ -38,6 +49,7 @@ public class Main {
                 break;
 
         }
+    }
 
         input.close();
     }
